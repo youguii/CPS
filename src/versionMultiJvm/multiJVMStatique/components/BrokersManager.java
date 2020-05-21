bc68dcd1bb6bdec1d3bf91b3103769a0f9374c31
KiskaAr <arabkaissa97@gmail.com>
2020-05-21 09:41:19 +0200

    new version multiJVM added

---
+++ src/versionMultiJvm/multiJVMStatique/components/BrokersManager.java
@@ -0,0 +1,150 @@
+package versionMultiJvm.multiJVMStatique.components;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.sorbonne_u.components.AbstractComponent;
+import fr.sorbonne_u.components.exceptions.ComponentShutdownException;
+import fr.sorbonne_u.components.exceptions.ComponentStartException;
+import fr.sorbonne_u.components.ports.InboundPortI;
+import fr.sorbonne_u.components.ports.OutboundPortI;
+import versionMultiJvm.multiJVMStatique.connectors.DistributionConnector;
+import versionMultiJvm.multiJVMStatique.interfaces.DistributionI;
+import versionMultiJvm.multiJVMStatique.ports.DistributionInboundPort;
+import versionMultiJvm.multiJVMStatique.ports.DistributionOutboundPort;
+
+public class	BrokersManager 
+extends		AbstractComponent
+implements		DistributionI{
+	
+	/** inbound port through which clients call this component.				*/
+	protected InboundPortI		inboundPort ;
+	
+	/** outbound ports through which this component calls brokers.			*/
+	protected OutboundPortI[]	outboundPorts ;
+	
+	/** a function mapping outbound ports to unique numbers.				*/
+	protected Map<OutboundPortI,Integer>	numbers ;
+
+	/** URIs of the inbound ports of the server to connect this component.	*/
+	protected String[]			brokerInboundPortURIs ;
+	
+	
+
+	public BrokersManager(
+			int nbThreads, 
+			int nbSchedulableThreads,
+			String ownInboundPortURI,
+			String[] brokerInboundPortURIs) throws Exception {
+		
+		
+		super(nbThreads, nbSchedulableThreads);
+
+		assert	nbThreads > 0 ;
+		assert	ownInboundPortURI != null ;
+		assert	brokerInboundPortURIs != null && brokerInboundPortURIs.length > 0 ;
+		this.init(ownInboundPortURI, brokerInboundPortURIs);
+											
+	}
+
+	
+	public BrokersManager(
+			String reflectionInboundPortURI, 
+			int nbThreads, 
+			int nbSchedulableThreads,
+			String ownInboundPortURI,
+			String[] brokerInboundPortURIs) throws Exception{
+		
+		super(reflectionInboundPortURI, nbThreads, nbSchedulableThreads);
+
+		assert	nbThreads > 0 ;
+		assert	ownInboundPortURI != null ;
+		assert	brokerInboundPortURIs != null && brokerInboundPortURIs.length > 0 ;
+		
+		this.init(ownInboundPortURI, brokerInboundPortURIs);
+	}
+	
+	
+
+	public void init (
+			String ownInboundPortURI,
+			String[] brokerInboundPortURIs) throws Exception {
+		
+		// Create InboundPort
+		this.inboundPort = new DistributionInboundPort(this);
+		assert	this.inboundPort instanceof DistributionI ;
+		this.inboundPort.publishPort() ;
+	
+		assert	this.inboundPort instanceof DistributionI ;
+		
+		// Create OutboundPorts
+		this.outboundPorts = new OutboundPortI[brokerInboundPortURIs.length] ;
+		this.numbers = new HashMap<OutboundPortI,Integer>() ;
+		
+		for (int i = 0 ; i < brokerInboundPortURIs.length ; i++) {
+			this.outboundPorts[i] = new DistributionOutboundPort(this) ;
+			this.numbers.put(this.outboundPorts[i], i) ;
+			assert	this.outboundPorts[i] instanceof DistributionI ;
+			this.outboundPorts[i].publishPort() ;
+		}
+		this.brokerInboundPortURIs = brokerInboundPortURIs ;
+		
+		
+		this.tracer.setTitle("BrokersManager") ;
+		this.tracer.setRelativePosition(1, 0) ;
+		this.toggleTracing() ;
+	}
+	
+	@Override
+	public void			start() throws ComponentStartException
+	{
+		super.start() ;
+		for (int i = 0 ; i < this.brokerInboundPortURIs.length ; i++) {
+			try {
+				this.doPortConnection(
+						this.outboundPorts[i].getPortURI(),
+						brokerInboundPortURIs[i],
+						DistributionConnector.class.getCanonicalName());
+			} catch (Exception e) {
+				throw new ComponentStartException(e) ;
+			}
+		}
+	}
+	
+	@Override
+	public void			finalise() throws Exception
+	{
+		for (int i = 0 ; i < this.outboundPorts.length ; i++) {
+			this.doPortDisconnection(this.outboundPorts[i].getPortURI()) ;
+		}
+		super.finalise() ;
+	}
+	
+	@Override
+	public void			shutdown() throws ComponentShutdownException
+	{
+		try {
+			for (int i = 0 ; i < this.outboundPorts.length ; i++) {
+				this.outboundPorts[i].unpublishPort() ;
+			}
+			this.inboundPort.unpublishPort() ;
+		} catch (Exception e) {
+			throw new ComponentShutdownException(e) ;
+		}
+		super.shutdown() ;
+	}
+	
+	
+	@Override
+	public void distribute(Object... parameters) throws Exception {
+		for (int i = 0 ; i <  this.outboundPorts.length ; i++) {
+			try {
+					((DistributionI)this.outboundPorts[i]).distribute(parameters) ;
+			} catch(Exception e) {
+				throw new Exception(e);
+			}
+		}
+		
+	}
+
+}
---
+++ src/versionMultiJvm/multiJVMStatique/interfaces/DistributionCI.java
@@ -0,0 +1,9 @@
+package versionMultiJvm.multiJVMStatique.interfaces;
+
+import fr.sorbonne_u.components.interfaces.OfferedI;
+import fr.sorbonne_u.components.interfaces.RequiredI;
+
+public interface DistributionCI 
+extends DistributionI, OfferedI, RequiredI {
+
+}
